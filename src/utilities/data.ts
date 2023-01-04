4dc006aff7e59295c9990083f3393f1228f649d1
gitonga <gitonga.deus@gmail.com>
2023-01-04 23:56:49 +0300

    updates and fixes

---
+++ src/utilities/data.ts
@@ -0,0 +1,61 @@
+export const data = {
+	about: {
+		name: "Deus Gitonga",
+		image: "https://avatars.githubusercontent.com/u/48915064?v=4",
+		position: "Software Developer",
+		phoneNumber: "+254 720 158 047",
+		locationAt: "Nairobi, Kenya",
+		introduction: [
+
+		]
+	},
+
+	links: [
+		{
+			name: "LinkedIn",
+			link: "https://linkedin.com/in/gitonga-deus/",
+		},
+		{
+			name: "GitHub",
+			link: "https://github.com/gitonga-deus"
+		},
+	],
+
+	experience: [
+		{
+			position: "Front End Developer",
+			company: "Freelance",
+			duration: "Nov 2019 - Present",
+			roles: [
+
+			]
+		},
+		{
+			position: "Jr. Front End Developer (Intern)",
+			company: "Kawelle Organization",
+			duration: "Apr 2019 - Aug 2019",
+			roles: [
+
+			]
+		}
+	],
+
+	education: [
+		{
+			degree: "Bachelor of Science in Mathematics & Computer Science",
+			school: "Jomo Kenyatta University of Agriculture and Technology",
+			duration: "Sept. 2018 - Jul. 2022",
+			achievements: [
+
+			]
+		}
+	],
+
+	skills: [
+
+	],
+
+	interests: [
+
+	]
+}
