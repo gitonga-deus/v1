694913b8bcd4cb0e537f7c0ad710f950f3368488
gitonga <gitonga.deus@gmail.com>
2023-01-08 01:05:08 +0300

    fixes

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Navigation } from "./components/Navigation";
+// import { About } from "./pages/About";
+
+export const App = () => {
+	return (
+		<div>
+			<Navigation />
+		</div>
+	)
+}
