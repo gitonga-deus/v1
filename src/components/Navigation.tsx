e55700ded9290237a1f669ece234eba8dc32d8b5
gitonga <gitonga.deus@gmail.com>
2023-01-08 01:04:34 +0300

    final update

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,7 +1,30 @@
-export const Navigation = () => { 
+import { Container, Nav, Button } from "react-bootstrap"
+
+export const Navigation = () => {
 	return (
-		<div>
-			Navigation
-		</div>
+		<header className="header-area">
+			<div className="main-menu">
+				<Nav className="navbar navbar-expand-lg navbar-light">
+					<Container>
+						<a href="/" className="navbar-brand">
+							<h2 className="logo-h">Deus Gitonga</h2>
+						</a>
+						<Button className="navbar-toggler" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle Navigation">
+							<span className="icon-bar"></span>
+							<span className="icon-bar"></span>
+							<span className="icon-bar"></span>
+						</Button>
+						<div className="collapse navbar-collapse offset" id="navbarSupportedContent">
+							<ul className="nav navbar-nav menu-nav justify-content-end">
+								<li className="nav-item active"><a className="nav-link" href="#">Home</a></li>
+								<li className="nav-item"><a className="nav-link" href="#">About</a></li>
+								<li className="nav-item"><a className="nav-link" href="#">Services</a></li>
+								<li className="nav-item"><a className="nav-link" href="#">Portfolio</a></li>
+							</ul>
+						</div>
+					</Container>
+				</Nav>
+			</div>
+		</header>
 	)
 }
