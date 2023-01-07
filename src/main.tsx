0f5b51ee6ff56152f30284db3548a95b1b54245d
gitonga <gitonga.deus@gmail.com>
2023-01-08 01:04:48 +0300

    minor changes and fixes

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Home } from './Home'
+import { App } from './App'
 
 import "bootstrap/dist/css/bootstrap.min.css"
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
-		<Home />
+		<App />
 	</React.StrictMode>
 )
