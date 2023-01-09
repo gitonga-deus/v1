9c95e0714baf186f813b361437a0065551f785ad
gitonga <gitonga.deus@gmail.com>
2023-01-09 17:32:21 +0300

    minor fix

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: [
-		"./index.html",
-		"./src/**/*.{js,ts,jsx,tsx}",
-	],
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		container: {
+			center: true,
+			padding: "4rem",
+		},
+		extend: {
+			colors: {
+				dark_primary: "#06223F",
+				bg_light_primary: "#F5F9FD",
+				gray: "#B7C5D3",
+			},
+			backgroundImage: {
+				primaryLinear: "linear-gradient(180deg, #B6CCF5 0.48%, #D5E3F1 100%)",
+			},
+			dropShadow: {
+				primary: "-5px 35px 40px rgba(223, 229, 236, 0.9)",
+			},
+		},
 	},
 	plugins: [],
-}
+};
